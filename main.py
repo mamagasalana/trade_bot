7c0a30e4c696dab0b7a65ef24aa51299ccfc672e
ytee <ytee@butterfly.com.hk>
2025-03-11 08:55:39 +0800

    preprocess version control(3) -- include vecm params, slicing mode

--- main.py
+++ main.py
@@ -12,20 +12,26 @@ def main():
     with open(config_path, "r") as f:
         config = json.load(f)
     
-    # Create a unique output folder using the current timestamp
-    folder_name = "test_" + datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-    os.makedirs(folder_name, exist_ok=True)
-    
+    # Read parameters from config
     script = config.get("script")
     pairs = config.get("pairs")
-    na_threshold = config.get("na_threshold", 0.9)
-    exclude_weekend = config.get("exclude_weekend", 1)
+    na_threshold = config.get("na_threshold")
+    exclude_weekend = config.get("exclude_weekend")
+    version = config.get("version")
+    slicing_mode = config.get("slicing_mode", "fix_start")
+    vecm_deterministic = config.get("vecm", {}).get("deterministic", "nc")
     
+    # Create a unique output folder using the current timestamp
+    folder_name = f"{version}_{'_'.join(pairs)}" + datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    os.makedirs(folder_name, exist_ok=True)
+
     main_params = [
         "--pairs", ",".join(pairs),
         "--na_threshold", str(na_threshold),
-        "--exclude_weekend", str(exclude_weekend), 
-        "--folder", folder_name
+        "--exclude_weekend", str(exclude_weekend),
+        "--folder", folder_name,
+        "--slicing_mode", slicing_mode,
+        "--vecm_deterministic", vecm_deterministic
     ]
     
     shutil.copy(script, folder_name)
@@ -35,7 +41,11 @@ def main():
     # Run test.py (its output will be saved to the current working directory)
     subprocess.run(["python", test_script] + main_params, check=True)
 
-    shutil.move(folder_name, 'output/')
+    output_dir = 'output'
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    shutil.move(folder_name, output_dir)
+
     print("Time taken: %s" % (time.time()-start))
 
 if __name__ == "__main__":
